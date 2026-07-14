974f4a6e770755a377f06c5520a2945b83aa960c
Mon <91261297+mon-jai@users.noreply.github.com>
2026-07-15 05:31:05 +0800

    refactor: update dependencies and add workarounds for `single-file-cli` issues

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
 import { resolve } from "path"
 
 // @ts-expect-error
-import { getHookScriptSource, getScriptSource, getZipScriptSource } from "single-file-cli/lib/single-file-script.js"
+import { hookScript, script, zipScript } from "single-file-cli/lib/single-file-bundle.js"
 
 // ["node", ".", "<path>"]
 export const WORKSPACE_ROOT = process.argv[2] ?? process.cwd()
 export const ARCHIVE_DIR = resolve(WORKSPACE_ROOT, "./conversations")
 
-export const SCRIPT = (await getScriptSource({})) + "; window.singlefile = singlefile"
-export const HOOK_SCRIPT = getHookScriptSource()
-export const ZIP_SCRIPT = getZipScriptSource()
+export const SCRIPT = script + "; window.singlefile = singlefile"
+export const HOOK_SCRIPT = hookScript
+export const ZIP_SCRIPT = zipScript
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { mkdir, unlink, writeFile } from "fs/promises"
 import { join, resolve } from "path"
 
-import { queue } from "async"
+import { asyncify, queue, retry } from "async"
 import { chromium } from "playwright"
 
 import { archiveConversation } from "./archive.ts"
@@ -26,11 +26,16 @@ for (const id of staleIds) {
 // Archive new conversations, at most 10 concurrently
 await using browser = await chromium.launch({ args: ["--disable-web-security"] })
 const archiveQueue = queue<string>(async id => {
-  try {
-    await archiveConversation(id, browser)
-  } catch (error) {
-    console.error(`Failed to archive ${id}: ${(error as Error).message}`)
-  }
+  // The latest version of the `single-file-cli` package introduced an error that happens occasionally
+  // Retry up to 10 times if it occurs
+  // Full error: {"instanceOf":"PlaywrightError","stack":"page.evaluate: SyntaxError: Failed to execute 'matches' on 'Element': '>:first-child' is not a valid selector.\n    at EL (<anonymous>:169:24500)\n    at SL (<anonymous>:169:24341)\n    at vL (<anonymous>:169:24211)\n    at bL (<anonymous>:169:23490)\n    at mL (<anonymous>:169:21180)\n    at dL (<anonymous>:169:20867)\n    at iL (<anonymous>:169:19724)\n    at bo (<anonymous>:169:19322)\n    at lL (<anonymous>:169:20577)\n    at iL (<anonymous>:169:19589)\n    at archiveConversation (D:\\GitHub\\archive\\src\\archive.ts:216:35)\n    at async file:///D:/GitHub/archive/src/index.ts:30:5","message":"page.evaluate: SyntaxError: Failed to execute 'matches' on 'Element': '>:first-child' is not a valid selector.\n    at EL (<anonymous>:169:24500)\n    at SL (<anonymous>:169:24341)\n    at vL (<anonymous>:169:24211)\n    at bL (<anonymous>:169:23490)\n    at mL (<anonymous>:169:21180)\n    at dL (<anonymous>:169:20867)\n    at iL (<anonymous>:169:19724)\n    at bo (<anonymous>:169:19322)\n    at lL (<anonymous>:169:20577)\n    at iL (<anonymous>:169:19589)","log":[],"name":"Error"}
+  const retryPromise = retry(
+    { times: 10, errorFilter: (error: Error) => error.message.includes(">:first-child") },
+    // `async.retry()` distinguishes between native async functions and regular functions
+    // For the latter, it assumes a Node-style callback
+    asyncify(() => archiveConversation(id, browser))
+  )
+  return retryPromise.catch(error => console.error(`Failed to archive ${id}: ${error.message}`))
 }, 10)
 archiveQueue.push(newIds)
 await archiveQueue.drain()
